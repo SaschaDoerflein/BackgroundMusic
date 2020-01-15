67906d2f45f1fece12234108fc0cfc8a7e37847b
Sascha <sascha_doerflein@web.de>
2020-01-15 19:54:51 +0100

    Add GetVersionNumber and show in title.

--- BackgroundMusic/MainWindow.xaml.cs
+++ BackgroundMusic/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,16 @@ namespace BackgroundMusic
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Title = "BackgroundMusic v" + GetVersionNumber();
+        }
+
+        private string GetVersionNumber()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+            return version;
         }
     }
 }
